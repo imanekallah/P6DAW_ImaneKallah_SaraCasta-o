068c899666fc08e28a94ed96de555c5886ed127c
imanekallah <imanekallah2@gmail.com>
2026-05-11 19:21:15 +0200

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -4,7 +4,11 @@ public class Wordle {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int opcion;
 
+		do{
+			System.out.println()
+		}while(opcion!=0);
 	}
 
 }
