56ede4b2e99eea4215b0512a2227d1982dac122e
imanekallah <imanekallah2@gmail.com>
2026-05-13 20:53:57 +0200

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -8,21 +8,25 @@ public class Wordle {
     public static final String ROJO = "\u001B[31m";
     public static final String GRIS = "\u001B[90m";
 
-	public static void titulo(){
-	System.out.println(AMARILLO+"          La Palabra del Día          "+RESET);	
-	}
-
-
-    
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
+
+    public static void menu(){
+		static Scanner sc = new Scanner(System.in);
 		int opcion;
+		    System.out.println(AMARILLO+"          La Palabra del Día          "+RESET);	
 
 		do{
-			titulo();
-			System.out.println("")
+			System.out.println("Selecciona una opción");
+			System.out.println("1- Cargar partida");
+			System.out.println("2- Nuevo juego");
+			System.out.println("3- Consultar puntuación");
+			System.out.println("4- Salir");
  		}while(opcion!=0);
+	}
+	
+	public static void main(String[] args) {
+		
 	}
 
 }
